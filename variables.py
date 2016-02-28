3db264fc7915e5a0b17e24681f72a7a34a2f1273
Andrey Egorov <andreyegorov@coherentsolutions.com>
2016-02-28 12:20:14 +0300

    Added PyCharm files to gitignore. Started to learn Python's Beauteful Heart.

---
+++ variables.py
@@ -0,0 +1,13 @@
+x = "blue"
+y = "green"
+z = x
+print(x, y, z)
+z = y
+print(x, y, z)
+x = z
+print(x, y, z)
+print("4th letter is", x[3])
+route = 866
+print(route, type(route))
+route = "North"
+print(route, type(route))
